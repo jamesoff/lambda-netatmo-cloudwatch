6c0d2782a894650478ad9dbe221da3cb71658642
James Seward <james@jamesoff.net>
2019-06-28 09:46:10 +0100

    Refactor lambda code

--- sam-app/netatmo/index.py
+++ sam-app/netatmo/index.py
@@ -1,17 +1,26 @@
+"""Fetch data from Netatmo and send it to CloudWatch."""
+
 import os
 import boto3
 import lnetatmo
 
 
 cloudwatch_client = boto3.client("cloudwatch")
+auth = lnetatmo.ClientAuth(username=os.environ["NETATMO_USERNAME"])
+
+
+def post_metrics(metric_data):
+    """Send prepared metric data to CloudWatch"""
+    cloudwatch_client.put_metric_data(Namespace="netatmo", MetricData=metric_data)
 
 
 def main():
-    auth = lnetatmo.ClientAuth(username=os.environ["NETATMO_USERNAME"])
+    """Fetch data from Netatmo and interate through it, posting to CloudWatch
+    Metrics"""
     weather_data = lnetatmo.WeatherStationData(auth)
-    last_data = weather_data.lastData("Kingston Road")
+    last_data = weather_data.lastData(os.environ["NETATMO_STATION"])
+    metric_data = []
     for device in last_data.keys():
-        metric_data = []
         device_data = last_data[device]
         when = device_data["When"]
         del device_data["When"]
@@ -25,10 +34,19 @@ def main():
                 "Value": value,
             }
             metric_data.append(metric)
-        cloudwatch_client.put_metric_data(Namespace="netatmo", MetricData=metric_data)
+            print("Number of metrics: {}".format(len(metric_data)))
+            if len(metric_data) > 19:
+                print("Posting")
+                post_metrics(metric_data)
+                metric_data = []
+    print("Loop finished, number of metrics to post: {}".format(metric_data))
+    if len(metric_data):
+        post_metrics(metric_data)
 
 
-def lambda_handler(event, contect):
+def lambda_handler(event, context):
+    """Handle invocation from Lambda. Currently this is the same as being run
+    directly, so just call main."""
     main()
 
 
